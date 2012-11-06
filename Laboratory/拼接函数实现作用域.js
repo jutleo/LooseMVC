a0e6858ae986183e7aec8f705bcbb269637bffba
Gaubee <Gaubeebangeel@gmail.com>
2012-11-06 20:08:49 +0800

    Update Laboratory/拼接函数实现作用域.js
    暂时完成了基本的功能，基于eval解析来实现。具体功能要基于项目去拓展，比如重写闭包，添加子运行域，链式子运行域。

--- Laboratory/拼接函数实现作用域.js
+++ Laboratory/拼接函数实现作用域.js
@@ -1,21 +1,43 @@
-var namespace=function(){
-  var content='';
-  var a=arguments,al=arguments.length;
-  for(var i=0;i<al;++i){
-    if(typeof a[i] == 'function'){
-      var fn=String(a[i]);
-      content+=fn.substr(11);
-    }else{ eval(a[i])}
-  }
-  alert(content);
-  namespace.content=namespace.content?namespace.content+content:content;
-eval(namespace.content);
-}; 
-namespace(30);
 
-namespace(function(){
-var x=90;
+var namespace=function(){
+ var NSpace={
+    content:'',
+    fn:function(){},
+     Cloud:function(evalStr){eval(evalStr);},
+     Get:function(arg){},
+    Run:function(){
+      this.fn='this.fn=function(arg){' +this.content +'this.Get=function(arg){if(typeof arg=="string"){return eval(arg);}};' +'return this.Get;' +'}';
+      alert(this.fn);
+      eval(this.fn);
+      this.fn();},
+    Add:function(fn){
+       if(typeof fn=='function'){
+         this.content+=String(fn).substr(11);
+       }
+     }
+  }
+
+if(typeof arguments[0]=='string'){
+  var ns=arguments[0].split('.');
+  var nsLink= this; //应用程序名或已知父级
+  var L=ns.length-1;
+  for(var i=0;i<L;++i) {
+      if(!nsLink[ns[i]]){ nsLink[ns[i]]={} ;}
+      nsLink=nsLink[ns[i]];
+    }
+   nsLink[ns[L]]=NSpace;
+  } 
+return NSpace;
+}; 
+var Test1=namespace('x.x.x');
+Test1.Add(function(){
+var x=80;
+alert(this);
 });
-namespace(function(){
+Test1.Add(function(){
 alert(x);
 });
+Test1.Run();
+//alert(Test1.Get);
+alert(Test1.Get('x'));
+alert(x.x.x.Get('x'))
